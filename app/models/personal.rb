20bd764e3fb6ba9ce3f3ae63b844a925376a0c16
Varsha Jaikumar <var2424@gmail.com>
2010-06-15 17:51:48 +0530

    fourth commit

--- app/models/personal.rb
+++ app/models/personal.rb
@@ -10,8 +10,8 @@ class Personal < ActiveRecord::Base
 	#validates_length_of :pin, :is => 6
  	#validates_length_of :mob, :is => 10
 	validates_inclusion_of :gender, :in => %w(M F), :message => "should be either M/F"
-	validates_numericality_of :pin
-	validates_numericality_of :mob
+	#validates_numericality_of :pin
+	#validates_numericality_of :mob
         validates_presence_of :name
 	
        
